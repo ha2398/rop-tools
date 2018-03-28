e56b19a11edd67c734af73a0ea82b376f710a0a6
Hugo Sousa <hsousa@gmx.com>
2018-03-27 22:16:58 -0300

    TestFramework: remove runtime limit from run_overhead_pintools

--- TestFramework/run_overhead_pintools.sh
+++ TestFramework/run_overhead_pintools.sh
@@ -7,20 +7,24 @@ PINTOOLS_PATH=$PIN_PATH/source/tools/SimpleExamples
 ROOT_DIR=${PWD}
 
 function compile_pintools() {
+	echo "### Compiling Pintools ###"
 	cp ../Pintools/Overhead/complete.cpp ../Pintools/Overhead/pruned.cpp $PIN_PATH/source/tools/SimpleExamples
 	cd $PINTOOLS_PATH
 	make clean
-	make obj-intel64/complete.so
-	make obj-intel64/pruned.so
+	make dir obj-intel64/complete.so
+	make dir obj-intel64/pruned.so
 	cd $ROOT_DIR
 }
 
 function run_pintools() {
-	COMPILE=0 EXEC=1 PIN=1 PINTOOL="complete" ./run.sh
+	echo "### Executing complete pintool ###"
+	COMPILE=0 EXEC=1 PIN=1 RUNTIME=0 PINTOOL="complete" ./run.sh
 	mv run.log complete.log
 	
-	COMPILE=0 EXEC=1 PIN=1 PINTOOL="pruned" ./run.sh
+	COMPILE=0 EXEC=1 PIN=1 RUNTIME=0  PINTOOL="pruned" ./run.sh
 	mv run.log pruned.log
+	echo "### Executing pruned pintool ###"
 }
 
 compile_pintools
+run_pintools
