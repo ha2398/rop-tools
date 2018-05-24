42b4547423f2bd87e2629c7a3bf4908753d5450c
Hugo Sousa <hsousa@gmx.com>
2018-05-24 08:16:15 -0300

    TestFramework: fixing bugs in pipe redirection

--- TestFramework/run_perf.sh
+++ TestFramework/run_perf.sh
@@ -7,10 +7,12 @@ while read -r LINE; do
 	cd_cmd=${cmds[0]}
 	bench_cmd=${cmds[2]}
 	
-	echo "$cd_cmd && echo \"Command: $bench_cmd\" && perf stat -r $REPEAT --log-fd 1 \
+	echo "$cd_cmd && 3> /dev/stdout perf stat -r $REPEAT --log-fd 3 \
 	       	$bench_cmd" >> $BASEDIR/run.txt.temp
 done < run.txt
 
 mv run.txt.temp run.txt
 
-parallel -j $JOBS < run.txt | grep "seconds time elapsed\|Command" | sed -r 's/,/./g' > run.log
+parallel -j $JOBS < run.txt > parallel.out
+cat parallel.out | grep "Performance counter stats\|seconds time elapsed" | sed -r 's/,/./g' > run.log
+rm -f parallel.out
