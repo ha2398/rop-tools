8ab3b85bcbdcacb671bad1483eda0de0b0fc3552
Hugo Sousa <hsousa@gmx.com>
2019-07-05 07:37:38 -0300

    Use all SPEC CPU2006 benchmarks

--- SPECCPU2006/endbr64/run.sh
+++ SPECCPU2006/endbr64/run.sh
@@ -20,8 +20,7 @@ CMDS_FILE="cmds.txt"
 
 PARALLEL_LOG="parallel.log"
 
-# declare -a benchs=("400.perlbench" "401.bzip2" "403.gcc" "429.mcf" "445.gobmk" "456.hmmer" "458.sjeng" "462.libquantum" "464.h264ref" "471.omnetpp" "473.astar" "483.xalancbmk" "999.specrand" "410.bwaves" "416.gamess" "433.milc" "434.zeusmp" "435.gromacs" "436.cactusADM" "437.leslie3d" "444.namd" "447.dealII" "450.soplex" "453.povray" "454.calculix" "459.GemsFDTD" "465.tonto" "470.lbm" "481.wrf" "482.sphinx3")
-declare -a benchs=("400.perlbench")
+declare -a benchs=("400.perlbench" "401.bzip2" "403.gcc" "429.mcf" "445.gobmk" "456.hmmer" "458.sjeng" "462.libquantum" "464.h264ref" "471.omnetpp" "473.astar" "483.xalancbmk" "999.specrand" "410.bwaves" "416.gamess" "433.milc" "434.zeusmp" "435.gromacs" "436.cactusADM" "437.leslie3d" "444.namd" "447.dealII" "450.soplex" "453.povray" "454.calculix" "459.GemsFDTD" "465.tonto" "470.lbm" "481.wrf" "482.sphinx3")
 
 function init() {
 	rm -rf endbr64_outputs
